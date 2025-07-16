1fd00859aa4bccb96a210dbef5d294c175c043e2
Abdullah Zia <abdullahkhanzia11@gmail.com>
2025-07-17 01:04:34 +0500

    Check

---
+++ week2/flight.py
@@ -0,0 +1,19 @@
+class flight:
+    def __init__ (self, cap):
+        self.cap = cap
+        self.pas = []
+
+    def booking(self, name):
+        if len(self.pas) < self.cap:
+            self.pas.append(name)
+            print(f"{name} added to flight successfully.")
+        else:
+            print(f"Limit Exceeded! Can not add {name}")
+
+f = flight(5)
+
+
+passengers = ["John", "Harry", "Ron", "Hermonie", "Doe", "Kris"]
+
+for p in passengers:
+    f.booking(p)
