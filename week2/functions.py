35b5c5b9a4d56d140e5da07a0084c79828aa40c3
Abdullah Zia <abdullahkhanzia11@gmail.com>
2025-07-17 01:07:09 +0500

    Check

--- week2/functions.py
+++ week2/functions.py
@@ -1,16 +1,30 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def square(x):
     return x*x
 
 for i in range(11):
     print(f"Square of {i} is {square(i)}")
 =======
+=======
+
+>>>>>>> f44eea3b4205293de7d7a9817f2f2d8430522e1b
 def squares(x):
     return x*x
 
 for i in range(11):
     print(f"Square of {i} is {squares(i)}")
+<<<<<<< HEAD
 >>>>>>> style
+=======
+
+def square(x):
+    return x*x
+
+for i in range(11):
+    print(f"Square of {i} is {square(i)}")
+
+>>>>>>> f44eea3b4205293de7d7a9817f2f2d8430522e1b
 
 print("Executed")
 
@@ -25,7 +39,11 @@ def show_info(**info):
 
 show_info(name="Ali", age=22)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+
+>>>>>>> f44eea3b4205293de7d7a9817f2f2d8430522e1b
 
 def product (*num):
     result  = 1
@@ -34,4 +52,8 @@ def product (*num):
     return result
 
 print(product(1,2,4))
+<<<<<<< HEAD
 >>>>>>> style
+=======
+
+>>>>>>> f44eea3b4205293de7d7a9817f2f2d8430522e1b
